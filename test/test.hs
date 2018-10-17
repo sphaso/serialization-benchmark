1321da0dd5bdcbc35a1b91625c00b1c8ec2ac8dd
sphaso <sphaso@gmail.com>
2018-10-17 20:54:47 +0200

    fixed test discovery

--- test/test.hs
+++ test/test.hs
@@ -1,7 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
 
 import Functions
 
@@ -15,23 +12,26 @@ import qualified Hedgehog.Range as Range
 
 stringToByteString :: Property
 stringToByteString = property $ do
-    xs <- forAll $ Gen.string (Range.linear 0 100) Gen.unicode
+    xs <- forAll $ Gen.string (Range.linear 0 10000) Gen.unicode
     (oldDecodeUtf8 . (oldEncodeUtf8 :: String -> B.ByteString)) xs === (newDecodeUtf8 . (newEncodeUtf8 :: String -> B.ByteString)) xs
 
 stringToLazyByteString :: Property
 stringToLazyByteString = property $ do
-    xs <- forAll $ Gen.string (Range.linear 0 100) Gen.unicode
+    xs <- forAll $ Gen.string (Range.linear 0 10000) Gen.unicode
     (oldDecodeUtf8 . (oldEncodeUtf8 :: String -> LB.ByteString)) xs === (newDecodeUtf8 . (newEncodeUtf8 :: String -> LB.ByteString)) xs
 
 tests :: IO Bool
 tests =
-    checkSequential $$(discover)
+  checkSequential $ Group "This module" [
+      ("strict", stringToByteString),
+      ("lazy", stringToLazyByteString)
+    ]
 
 main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
   hSetBuffering stderr LineBuffering
 
-  _ <- seq () tests
+  _ <- tests
 
   return ()
